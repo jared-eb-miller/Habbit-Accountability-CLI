fe37ec016dc5f2b368ae397288b5ff2ff98da497
Jared Miller <jebm423@gmail.com>
2023-12-18 17:56:17 -0600

    refactor 2

--- cli.py
+++ cli.py
@@ -4,33 +4,23 @@ import os
 def add_log_to(cat: Category):
     print(f"Adding log to category {cat}")
 
-def create_category():
-    c = Category.new()
-    add_log_to(c)
-
 def add_log():
     os.chdir(os.path.dirname(__file__) + "/resources/category-logs/")
     category_log_file_names: list[str] = os.listdir()
     category_names: list[str] = [fn[:fn.find('.')] for fn in category_log_file_names]
     options: list[str] = category_names + ["Create new category"]
 
-    options_menu(
-        title="Choose a Category to Log",
-        options=options
+    choice = which(
+        options,
+        title="Choose a Category to Log"
     )
 
-    choice = int(input("Please enter your choice: "))
-
-    if choice == len(options):
-        create_category()
+    if choice == len(options)-1:
+        c = Category.new()
     else:
-        if choice < 0:
-            raise ValueError("Invalid choice.")
-        
-        chosen_category_log_file_name = category_log_file_names[choice-1]
-
-        with open(chosen_category_log_file_name, 'r') as f:
+        with open(category_log_file_names[choice], 'r') as f:
             c = Category.from_json_file(f)
-        add_log_to(c)
+
+    add_log_to(c)
 
 add_log()
