080b662a7c3f24c7cb5bbeaa92a6397cc44b848f
Jared Miller <jebm423@gmail.com>
2023-12-20 19:56:38 -0600

    v1.0

--- cli.py
+++ cli.py
@@ -1,8 +1,55 @@
 from resources import *
 import os
+from datetime import datetime
 
-def add_log_to(cat: Category):
-    print(f"Adding log to category {cat}")
+@pad_vertical
+def new_category() -> Category:
+    c = Category(input("Please enter the name for the category: "))
+    print()
+    print("Please enter the name of all the customm fields for your behavior category:")
+    print("                          Enter 'DONE' to finsih")
+
+    i = 0
+    while True:
+        i += 1
+        f = input(f"field [{i}]: ")
+        if f == "DONE":
+            break
+        c.add_custom_field(Field(f, Field.TYPE_DEFAULT))
+    
+    if bool_prompt("Would you like to change the type of any field?"):
+        fields: list[Field] = list(c.custom_fields)
+        while True:
+            field = fields[which([str(f) for f in fields])]
+            try:
+                field.set_type(Field.TYPE_REPR_DICT[input("Enter the nwe field type: ")])
+            except KeyError:
+                print("Error. Given field type not supported.")
+                break
+
+    return c
+
+def get_log_content(cat: Category) -> list[Field]:
+    print(heading(f"New {cat.name} Entry"))
+    print()
+
+    content: list[Field] = []
+    for i, field in enumerate(cat.fields):
+        content.append(
+            field.partial_copy().set_data(
+                error_check(
+                    _input=input(f"Field [{i}] {field.id} {repr(field.t)}: "),
+                    _type=field.t,
+                    err_msg=f"Invalid {repr(field.t)}."
+                )
+            )
+        )
+    
+    # check if empty date
+    if content[-1].data == "":
+        content[-1].data = str(datetime.now())
+    
+    return content
 
 def add_log():
     os.chdir(os.path.dirname(__file__) + "/resources/category-logs/")
@@ -16,11 +63,16 @@ def add_log():
     )
 
     if choice == len(options)-1:
-        c = Category.new()
+        c = new_category()
     else:
         with open(category_log_file_names[choice], 'r') as f:
-            c = Category.from_json_file(f)
+            c = Category.from_json_file(f, category_names[choice])
 
-    add_log_to(c)
+    c.add_log_entry(LogEntry(
+        date=str(datetime.now()),
+        content=get_log_content(c)
+    ))
+
+    c.save()
 
 add_log()
--- resources.py
+++ resources.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from math import floor, ceil
 from json import load, dump
+from datetime import datetime
 
 
 INDENT_WIDTH: int = 3
@@ -9,99 +10,127 @@ INDENT_STR: str = " "*INDENT_WIDTH
 
 class ChoiceError(ValueError): pass
 
-class ReprWrapper(object):
-    def __repr__(self) -> str:
-        ts = str(self)
-        return "({})".format(ts[ts[:-3].rfind("'")+1:-2])
-    
 @dataclass
 class BassField:
     id: str
     t: type
+    data = None
 
 class Field(BassField):
     TYPE_DEFAULT: type = str
     SUPPORTED_TYPES = (str, int, float)
     TYPE_REPR_DICT = {}
     for t in SUPPORTED_TYPES:
-        TYPE_REPR_DICT.update({ str(ReprWrapper(t)): t })
+        TYPE_REPR_DICT.update({ str(repr(t)): t })
 
     def __repr__(self):
-        return f"<Field {ReprWrapper(self.t)} '{self.id}'>"
+        return f"<Field {repr(self.t)} '{self.id}'>"
     
     def set_type(self, t: type) -> None:
         self.t = t
 
+    def set_data(self, data) -> Field:
+        self.data = data
+        return self
+
+    def get_data(self):
+        return self.data
+    
+    def partial_copy(self):
+        return Field(self.id, self.t)
+
+    def full_copy(self):
+        c = Field(self.id, self.t)
+        return c.set_data(self.data)
+    
+@dataclass
+class LogEntry:
+    date: datetime
+    content: list[Field]
+
 class Category:
-    BASE_FILEDS: tuple[Field] = (Field("date", str), Field("description", str))
+    BASE_FILEDS: tuple[Field] = (Field("description", str), Field("date", str))
 
     def __init__(self, name: str, custom_fields: tuple[Field] = ()):
         self.name = name
 
-        self._custom_fields = custom_fields
+        self.custom_fields = custom_fields
         self._base_fields = Category.BASE_FILEDS
         self.fields: tuple[Field] = custom_fields + Category.BASE_FILEDS
 
+        self.entries: dict = {}
+
     def __add_field(self, field: Field) -> None:
-        self.fields += (field,)
+        self.fields = (field,) + self.fields
 
     def add_custom_field(self, field: Field) -> None:
-        self._custom_fields += (field,)
+        self.custom_fields += (field,)
         self.__add_field(field)
+
+    def add_log_entry(self, log_entry: LogEntry) -> None:
+        content: dict = {}
+        for field in log_entry.content:
+            content.update({ field.id: field.data })
+        self.entries.update({ log_entry.date: content })
+
+    def save(self) -> None:
+        with open(f"{self.name}.json", 'w') as f:
+            dump(self.entries, f, indent=4)
 
     def __repr__(self):
         return f"<Category '{self.name}' with fields {self.fields}>"
     
-    def new() -> Category:
-        c = Category(input("Please enter the name of your behavior category: "))
-        print("Please enter the name of all the customm fields for your behavior category:")
-        print("                          Enter 'DONE' to finsih")
-
-        i = 0
-        while True:
-            i += 1
-            f = input(f"field [{i}]: ")
-            if f == "DONE":
-                break
-            c.add_custom_field(Field(f, Field.TYPE_DEFAULT))
-        
-        if bool_prompt("Would you like to change the type of any field?"):
-            fields: list[Field] = list(c.fields)
-            while True:
-                field = fields[which(fields)]
-                try:
-                    field.set_type(Field.TYPE_REPR_DICT[input("Enter the nwe field type: ")])
-                except KeyError:
-                    print("Error. Given field type not supported.")
-                    break
-
+    def from_json_file(fs, name: str) -> Category:
+        _dict: dict = load(fs)
+        c = Category(name)
+        c.entries = _dict
+        field_name_strs: list[str]= _dict[tuple(_dict.keys())[0]].keys()
+        c.fields: tuple[Field] = ()
+        for field_name_str in field_name_strs:
+            c.fields += (Field(field_name_str, str),)
         return c
     
-    def from_json_file(fs) -> Category:
-        _dict = load(fs)
+def repr(t: type):
+    ts = str(t)
+    return "({})".format(ts[ts[:-3].rfind("'")+1:-2])
+
+def pad_vertical(fn: function):
+    def wrapper(*args, **kwargs):
+        print()
+        return fn(*args, **kwargs)
+    return wrapper
+
+CHAR = "*"
+BAR_LEN = 58
+BAR = CHAR*BAR_LEN
+
+def heading(title: str | None = None) -> str:
+    if title is None:
+        return BAR
     
+    sidebars_len = (BAR_LEN - len(title) - 2) / 2
+    s = CHAR*floor(sidebars_len)
+    s += " " + title + " "
+    s += CHAR*ceil(sidebars_len)
+    
+    return s
 
 def options_menu(title: str, options: list[str]) -> None:
-    CHAR = "*"
-    BAR_LEN = 58
-    BAR = CHAR*BAR_LEN
     indent_level = \
         2 if max([len(s.strip()) for s in options]) <= (BAR_LEN - INDENT_WIDTH*2 - 3) \
         else 1
-    sidebars_len = (BAR_LEN - len(title) - 2) / 2
-    s =  CHAR*floor(sidebars_len)
-    s += " " + title + " "
-    s += CHAR*ceil(sidebars_len)
-    s += "\n\n"
+    
+    s = heading(title) + "\n\n"
     for i, o in enumerate(options, 1):
         s += INDENT_STR*indent_level + str(i) + ") " + o + "\n"
     s += "\n" + BAR
     print(s)
 
+NONE = lambda *args: True
 def error_check(
         _input: str, 
         _type: type, 
-        condition: function, 
+        condition: function = NONE, 
         err_msg: str = "Invalid response."
         ):
     try:
@@ -132,6 +161,6 @@ def which(options: list[str], title: str = "Which?") -> int:
     return error_check(
         input("Please enter your choice: "),
         int,
-        lambda x: x > 0 and x < len(options),
+        lambda x: x > 0 and x <= len(options),
         "Invalid chioce."
     ) - 1
